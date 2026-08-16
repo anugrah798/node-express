6880b80fc94ddc73d13d85d645168034df714590
Anugrah T <tanugrah798@gmail.com>
2026-08-16 17:47:14 +0530

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!")
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Home page')
+})
+
+app.get('/about-us', (req, res) => {
+    res.send('About page')
+})
+
+
+app.get('/contact-us', (req, res) => {
+    res.send('Contact page')
+})
+
+app.listen(5000, () => {
+    console.log('server Running')
+})
